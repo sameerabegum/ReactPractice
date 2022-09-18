549a589f527b8381a517afd4999a6efe42836536
sameerabegum <35399063+sameerabegum@users.noreply.github.com>
2022-09-18 23:09:28 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="mainDiv">
+      <h1></h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-qrlgxz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-qrlgxz)
